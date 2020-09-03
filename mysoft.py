65f814044c1865d457a7eeab8e6835b98ded3233
priyanka prasad <59612128+P-riyanka-prasad@users.noreply.github.com>
2020-09-03 10:07:10 +0530

    Update mysoft.py

--- mysoft.py
+++ mysoft.py
@@ -1,44 +1,90 @@
 import pyttsx3
 import os
+import speech_recognition as SRG
+import time
+import webbrowser
+engine=pyttsx3.init()
 
-pyttsx3.speak("hi there you can chat with me as per your requirements")
-print()
+print('''
+ 
+ 
+----------------------------------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------------------------
+
+                                     *******    ********      **   **       ****** 
+                                     ********      **         **  **       ********
+                                     **    **      **         ** **        **    **
+                                     **    **      **         ****         **    **
+                                     *******       **         ****         ********
+                                     **            **         ** **        **    **
+                                     **            **         **  **       **    **
+                                     **         ********      **   **      **    **
+                                     
+----------------------------------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------------------------
+
+ 
+''')
+pyttsx3.speak("hi there you can talk with me")
+print("hi there you can talk with me")
+pyttsx3.speak('what do you want me to do?')
+
 
 while True:
-    pyttsx3.speak('what do you want me to do?')
-    print("chat with me as per your requirements: ",end='')
-    p=input()
-    if (("run" in p) or ("launch" in p) or ("execute"in p) or ("open" in p)) and ("notepad"in p):
+    variable = SRG.Recognizer()
+    with SRG.Microphone() as source:
+        print('what do you want me to do?')
+        engine.runAndWait()
+        
+        audio_input = variable.record(source,duration=6)
+        try:
+            text = variable.recognize_google(audio_input)
+            print(text)
+        except:
+            print(' Could not process audio...')
+            pyttsx3.speak('Sorry Could not process audio')
+            break
+    if ('introduce' in text) or ('yourself' in text) :
+        print('menu')
+        pyttsx3.speak("myself pika your voice assistant developed by priyanka just  just tell me what do you want me to do mam")
+    
+    elif (("run" in text) or ("launch" in text) or ("execute"in text) or ("open" in text)) and (("notepad"in text)or("Notepad"in text)):
         pyttsx3.speak("opening notepad")
         os.system("notepad")
         
-    elif(("run" in p)or("launch" in p)or("execute"in p)or("open" in p)) and ("wmplayer"in p):
+    elif(("run" in text)or("launch" in text)or("execute"in text)or("open" in text)) and (("media player"in text)or("media"in text)or("Music"in text)):
         pyttsx3.speak("opening wmplayer")
         os.system("wmplayer")
         
-    elif(("run" in p)or("launch" in p)or("execute"in p)or("open" in p)) and ("chrome"in p):
+    elif(("run" in text)or("launch" in text)or("execute"in text)or("open" in text)) and (("chrome"in text)or("Chrome"in text)):
         pyttsx3.speak("opening chrome")
         os.system("chrome")
         
-    elif(("run" in p)or("launch" in p)or("execute"in p)or("open" in p)) and ("BurpSuite"in p): 
+    elif(("run" in text)or("launch" in text)or("execute"in text)or("open" in text)) and (("BurpSuite"in text)or("Burp"in text)): 
         pyttsx3.speak("opening burpsuit")
         os.system("BurpSuiteCommunity")
         
-    elif(("run" in p)or("launch" in p)or("execute"in p)or("open" in p)) and ("calculator" in p): 
-        pyttsx3.speak("opening calculator")
+    elif(("run" in text)or("launch" in text)or("execute"in text)or("open" in text)) and (("claculator"in text)or("cal"in text)): 
+        pyttsx3.speak("opening claculator")
         os.system("calc")
         
-    elif(("run" in p)or("launch" in p)or("execute"in p)or("open" in p)) and ("prompt"in p): 
+    elif(("run" in text)or("launch" in text)or("execute"in text)or("open" in text)) and (("prompt"in text)or("command"in text)): 
         pyttsx3.speak("opening command prompt")
         os.system("cmd")
         
-    elif(("run" in p)or("launch" in p)or("execute"in p)or("open" in p)) and ("BurpSuite"in p): 
+    elif(("run" in text)or("launch" in text)or("execute"in text)or("open" in text)) and ("BurpSuite"in text): 
         pyttsx3.speak("opening burpsuit")
         os.system("BurpSuiteCommunity")
         
-    elif("exit"in p)or("quit"in p):
+    elif(("google" in text) or ("search" in text))and(("myself"in text)or("my name"in text)or("name")):
+        pyttsx3.speak("googling  stuffs")
+        webbrowser.open('https://priyankaprasad2.bookmark.com/', new = 2)
+        
+    elif("exit"in text)or("quit"in text):
         pyttsx3.speak("thank you i am sure developer will add more features later to me")
         break
+        
     else:
         print("invalid request")
+        pyttsx3.speak("command not supported ")
 print()
